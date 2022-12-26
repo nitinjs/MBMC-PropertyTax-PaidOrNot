619a43ea50b6734beb69f1197c27373e8ff7b576
nitinjs <nitin@nitinsawant.com>
2022-12-26 06:26:23 +0530

    stable-intermediate

--- index.js
+++ index.js
@@ -10,27 +10,63 @@ import $ from 'jquery';
 //appDiv.innerHTML = `<h1>Check whether property tax paid?</h1>`;
 
 var baseURL = 'https://pg.mbmc.gov.in';
-function GetPropertyTax(propertyCode){
-  var result="";
+function GetPropertyTax(propertyCode) {
+  var result = '';
   $.ajax({
     type: 'POST',
     contentType: 'application/json',
-    async: false, 
+    async: false,
     url: baseURL + '/merchantpg/api/getBill',
     data: JSON.stringify(data),
     dataType: 'json',
     success: function (res) {
       //console.log(res);
-      result = res; 
+      result = res;
       //if (responseText.responseStatus == 300) {
       //}
     },
   });
+  console.log(result);
   return result;
 }
 var data = { channel: 0, code: 'K040032082039' };
 $(document).ready(function () {
-  $("#txtPropCodes").on("keypress", function(evt){
-    
+  $('#txtPropCodes').on('change', function (evt) {
+    console.log($(this).val());
+    $('.invalid-feedback, .valid-feedback').hide();
+    if ($('#txtPropCodes').val() != '') {
+      $('#txtPropCodes').addClass('is-valid').removeClass('is-invalid');
+      $('.valid-feedback').show();
+    } else {
+      $('#txtPropCodes').addClass('is-invalid').removeClass('is-valid');
+      $('.invalid-feedback').show();
+    }
+  });
+  $('#btnSumbit').on('click', function () {
+    if ($('#txtPropCodes').hasClass('is-valid')) {
+      $('.table tr').not('.loading, .head').remove();
+      $('.table').show();
+      var num = $('#txtPropCodes').val();
+      var arr = num.split(',');
+      console.log(arr);
+      $.each(arr, function (i, e) {
+        var json = GetPropertyTax(e);
+        var html = $('<tr></tr>');
+        html.append($('<td>' + json.bill_details.propertyCode + '</td>'));
+        html.append($('<td>' + json.bill_details.ownerName + '</td>'));
+        html.append($('<td>₹' + json.bill_details.billAmount + '</td>'));
+        if (json.bill_details.billAmount == 0) {
+          html.append(
+            $('<td><span class="alert alert-success">✔️</span></td>')
+          );
+        } else {
+          html.append($('<td><span class="alert alert-danger">❌</span></td>'));
+        }
+        $('#tblResult').append(html);
+      });
+      $('.table tr.loading').hide();
+    } else {
+      $('.table').hide();
+    }
   });
 });
