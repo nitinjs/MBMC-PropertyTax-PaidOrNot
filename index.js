ee9a59a5e3c59e19e175b190db2775357153a0c0
nitinjs <nitin@nitinsawant.com>
2022-12-26 05:45:47 +0530

    input form

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ import $ from 'jquery';
 //console.log($);
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Check whether property tax paid?</h1>`;
+//const appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>Check whether property tax paid?</h1>`;
 
 var baseURL = 'https://pg.mbmc.gov.in';
-var data = { channel: 0, code: 'K040032082039' };
-$(document).ready(function () {
+function GetPropertyTax(propertyCode){
+  var result="";
   $.ajax({
     type: 'POST',
     contentType: 'application/json',
+    async: false, 
     url: baseURL + '/merchantpg/api/getBill',
     data: JSON.stringify(data),
     dataType: 'json',
-    success: function (responseText) {
-      console.log(responseText);
-
-      if (responseText.responseStatus == 300) {
-      }
+    success: function (res) {
+      //console.log(res);
+      result = res; 
+      //if (responseText.responseStatus == 300) {
+      //}
     },
+  });
+  return result;
+}
+var data = { channel: 0, code: 'K040032082039' };
+$(document).ready(function () {
+  $("#txtPropCodes").on("keypress", function(evt){
+    
   });
 });
