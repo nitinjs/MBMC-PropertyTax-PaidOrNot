2ecca8eea380de3c33ba334d339aec97755a6109
Nitin <nitin.sawant@outlook.com>
2023-01-30 03:30:15 +0530

    init email form design

---
+++ DesktopApp/PropTaxApp/frmMain.Designer.cs
@@ -0,0 +1,303 @@
+﻿using ManiX;
+namespace PropTaxApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            ManiX.Office2010Blue office2010Blue1 = new ManiX.Office2010Blue();
+            ManiX.Office2010Red office2010Red1 = new ManiX.Office2010Red();
+            this.rendererManager1 = new Office2007Renderer.RendererManager(this.components);
+            this.ntfTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.xButton1 = new ManiX.XButton();
+            this.themedLabel1 = new Office2007Renderer.ThemedLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.xButton3 = new ManiX.XButton();
+            this.themedLabel2 = new Office2007Renderer.ThemedLabel();
+            this.xButton2 = new ManiX.XButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ctxTray.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rendererManager1
+            // 
+            this.rendererManager1.Palettes = Office2007Renderer.Palette.Office2007Blue;
+            this.rendererManager1.Renderers = Office2007Renderer.Renderer.Standard;
+            // 
+            // ntfTray
+            // 
+            this.ntfTray.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.ntfTray.BalloonTipText = "E-Mail Settings";
+            this.ntfTray.BalloonTipTitle = "PropTaxApp";
+            this.ntfTray.ContextMenuStrip = this.ctxTray;
+            this.ntfTray.Icon = ((System.Drawing.Icon)(resources.GetObject("ntfTray.Icon")));
+            this.ntfTray.Visible = true;
+            // 
+            // ctxTray
+            // 
+            this.ctxTray.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ctxTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnSettings,
+            this.mnExit});
+            this.ctxTray.Name = "ctxTray";
+            this.ctxTray.Size = new System.Drawing.Size(132, 52);
+            // 
+            // mnSettings
+            // 
+            this.mnSettings.Name = "mnSettings";
+            this.mnSettings.Size = new System.Drawing.Size(131, 24);
+            this.mnSettings.Text = "&Settings";
+            // 
+            // mnExit
+            // 
+            this.mnExit.Name = "mnExit";
+            this.mnExit.Size = new System.Drawing.Size(131, 24);
+            this.mnExit.Text = "&Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 32);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(470, 211);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.xButton1);
+            this.groupBox1.Controls.Add(this.themedLabel1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 310);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Property Codes";
+            // 
+            // xButton1
+            // 
+            this.xButton1.AutoSize = true;
+            office2010Blue1.BorderColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010Blue1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010Blue1.ButtonMouseOverColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Blue1.ButtonMouseOverColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Blue1.ButtonMouseOverColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(137)))));
+            office2010Blue1.ButtonMouseOverColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(249)))), ((int)(((byte)(224)))));
+            office2010Blue1.ButtonNormalColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010Blue1.ButtonNormalColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010Blue1.ButtonNormalColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(97)))), ((int)(((byte)(181)))));
+            office2010Blue1.ButtonNormalColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(125)))), ((int)(((byte)(219)))));
+            office2010Blue1.ButtonSelectedColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Blue1.ButtonSelectedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Blue1.ButtonSelectedColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(117)))));
+            office2010Blue1.ButtonSelectedColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(216)))), ((int)(((byte)(107)))));
+            office2010Blue1.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Blue1.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Blue1.TextColor = System.Drawing.Color.White;
+            this.xButton1.ColorTable = office2010Blue1;
+            this.xButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.xButton1.Location = new System.Drawing.Point(399, 25);
+            this.xButton1.Name = "xButton1";
+            this.xButton1.Size = new System.Drawing.Size(56, 35);
+            this.xButton1.TabIndex = 9;
+            this.xButton1.Text = "Add";
+            this.xButton1.Theme = ManiX.Theme.MSOffice2010_BLUE;
+            this.xButton1.UseVisualStyleBackColor = true;
+            // 
+            // themedLabel1
+            // 
+            this.themedLabel1.AutoSize = true;
+            this.themedLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.themedLabel1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.themedLabel1.ForeColor = System.Drawing.Color.White;
+            this.themedLabel1.Location = new System.Drawing.Point(23, 32);
+            this.themedLabel1.Name = "themedLabel1";
+            this.themedLabel1.Size = new System.Drawing.Size(153, 23);
+            this.themedLabel1.TabIndex = 8;
+            this.themedLabel1.Text = "Property Tax Code:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.xButton3);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(12, 337);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(516, 95);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "E-Mail Settings";
+            // 
+            // xButton3
+            // 
+            this.xButton3.AutoSize = true;
+            this.xButton3.ColorTable = office2010Blue1;
+            this.xButton3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.xButton3.Location = new System.Drawing.Point(23, 39);
+            this.xButton3.Name = "xButton3";
+            this.xButton3.Size = new System.Drawing.Size(211, 35);
+            this.xButton3.TabIndex = 0;
+            this.xButton3.Text = "Connect to google";
+            this.xButton3.Theme = ManiX.Theme.MSOffice2010_BLUE;
+            this.xButton3.UseVisualStyleBackColor = true;
+            // 
+            // themedLabel2
+            // 
+            this.themedLabel2.AutoSize = true;
+            this.themedLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.themedLabel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.themedLabel2.ForeColor = System.Drawing.Color.White;
+            this.themedLabel2.Location = new System.Drawing.Point(13, 453);
+            this.themedLabel2.Name = "themedLabel2";
+            this.themedLabel2.Size = new System.Drawing.Size(392, 20);
+            this.themedLabel2.TabIndex = 8;
+            this.themedLabel2.Text = "You will not recieve email until google oauth is connected";
+            // 
+            // xButton2
+            // 
+            this.xButton2.AutoSize = true;
+            office2010Red1.BorderColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010Red1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010Red1.ButtonMouseOverColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Red1.ButtonMouseOverColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Red1.ButtonMouseOverColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(137)))));
+            office2010Red1.ButtonMouseOverColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(249)))), ((int)(((byte)(224)))));
+            office2010Red1.ButtonNormalColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(77)))), ((int)(((byte)(45)))));
+            office2010Red1.ButtonNormalColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(148)))), ((int)(((byte)(64)))));
+            office2010Red1.ButtonNormalColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(77)))), ((int)(((byte)(45)))));
+            office2010Red1.ButtonNormalColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(148)))), ((int)(((byte)(64)))));
+            office2010Red1.ButtonSelectedColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Red1.ButtonSelectedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Red1.ButtonSelectedColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(117)))));
+            office2010Red1.ButtonSelectedColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(216)))), ((int)(((byte)(107)))));
+            office2010Red1.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Red1.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Red1.TextColor = System.Drawing.Color.White;
+            this.xButton2.ColorTable = office2010Red1;
+            this.xButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.xButton2.Location = new System.Drawing.Point(460, 446);
+            this.xButton2.Name = "xButton2";
+            this.xButton2.Size = new System.Drawing.Size(68, 35);
+            this.xButton2.TabIndex = 9;
+            this.xButton2.Text = "Close";
+            this.xButton2.Theme = ManiX.Theme.MSOffice2010_RED;
+            this.xButton2.UseVisualStyleBackColor = true;
+            this.xButton2.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 489);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 18);
+            this.panel1.TabIndex = 10;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::PropTaxApp.Properties.Resources.frmBG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(544, 507);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.xButton2);
+            this.Controls.Add(this.themedLabel2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Settings";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ctxTray.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Office2007Renderer.RendererManager rendererManager1;
+        private System.Windows.Forms.NotifyIcon ntfTray;
+        private System.Windows.Forms.ContextMenuStrip ctxTray;
+        private System.Windows.Forms.ToolStripMenuItem mnSettings;
+        private System.Windows.Forms.ToolStripMenuItem mnExit;
+        private System.Windows.Forms.TextBox textBox1;
+        
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+       
+        private Office2007Renderer.ThemedLabel themedLabel1;
+        private Office2007Renderer.ThemedLabel themedLabel2;
+        private XButton xButton1;
+        private XButton xButton2;
+        private XButton xButton3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DesktopApp/PropTaxApp/frmMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropTaxApp
+{
+    public partial class frmMain : Form
+    {
+        bool runInBg = true;
+        public frmMain()
+        {
+            InitializeComponent();
+            mnExit.Click += MnExit_Click;
+            mnSettings.Click += MnSettings_Click;
+            ntfTray.DoubleClick += MnSettings_Click; ;
+        }
+
+        private void MnSettings_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void MnExit_Click(object sender, EventArgs e)
+        {
+            runInBg = false;
+            this.Close();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            runInBg = true;
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            e.Cancel = runInBg;
+        }
+    }
+}
